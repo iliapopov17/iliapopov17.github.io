82f57f70dfcdadc6327ed92893e968223def42f7
Ilia Popov <iljapopov17@gmail.com>
2026-04-21 21:38:41 +0200

    Revert to last stable version

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -15,13 +15,12 @@ var breaks = [];
 function updateNav() {
 
   var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
-  var vlinksWidth = $vlinks[0].scrollWidth;
 
   // The visible list is overflowing the nav
-  if(vlinksWidth > availableSpace) {
+  if($vlinks.width() > availableSpace) {
 
     // Record the width of the list
-    breaks.push(vlinksWidth);
+    breaks.push($vlinks.width());
 
     // Move item to the hidden list
     $vlinks.children().last().prependTo($hlinks);
@@ -53,7 +52,7 @@ function updateNav() {
   $btn.attr("count", breaks.length);
 
   // Recur if the visible list is still overflowing the nav
-  if($vlinks[0].scrollWidth > availableSpace) {
+  if($vlinks.width() > availableSpace) {
     updateNav();
   }
 
